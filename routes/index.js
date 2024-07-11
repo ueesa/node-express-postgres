213a1223f88b4ac9ff5ae266ffb95381f131826a
ueesa <143480538+ueesa@users.noreply.github.com>
2024-07-11 07:12:11 +0000

    yes

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,48 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-
-const connection = new pg.Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'postgres',
-  database: 'todo_app',
-  port:5432
-});
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      console.log(error);
-      console.log(results.rows);
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
       res.render('index', {
         title: 'ToDo App',
         todos: results.rows,
+        isAuth: isAuth,
       });
-    }
-  );
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
+      });
+    });
 });
 
 router.post('/', function (req, res, next) {
-  connection.connect((err) => {
-    if (err) {
-      console.log('error connecting: ' + err.stack);
-      return
-    }
-    console.log('success');
-  });
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, '${todo}');`,
-    (error, results) => {
-      console.log(error);
-      res.redirect('/');
-    }
-  );
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
+      });
+    });
 });
+
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;

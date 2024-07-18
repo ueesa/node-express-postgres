d9e8bd509bb69610371a7fa52dfdedc55fe53462
ueesa <143480538+ueesa@users.noreply.github.com>
2024-07-18 06:47:05 +0000

    git

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });

694587c22ba3f9161aa4aff7331a4a9ed2f91764
ueesa <143480538+ueesa@users.noreply.github.com>
2024-07-19 10:24:19 +0000

    yes

--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-  req.session = null;
-  res.redirect('/');
-});
-
-module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const knex = require("../db/knex");
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
   res.render('signup', {
@@ -50,4 +50,5 @@ router.post('/', function (req, res, next) {
       });
     });
 });
+
 module.exports = router;

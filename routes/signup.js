17ee06a6e8f296bcc2ccf95c9625e856f8363045
ueesa <143480538+ueesa@users.noreply.github.com>
2024-07-25 06:27:02 +0000

    yes

--- routes/signup.js
+++ routes/signup.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   res.render('signup', {
+    isAuth: isAuth,
     title: 'Sign up',
   });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -19,6 +24,7 @@ router.post('/', function (req, res, next) {
     .then(function (result) {
       if (result.length !== 0) {
         res.render("signup", {
+          isAuth: isAuth,
           title: "Sign up",
           errorMessage: ["このユーザ名は既に使われています"],
         }) 
@@ -31,12 +37,14 @@ router.post('/', function (req, res, next) {
           .catch(function (err) {
             console.error(err);
             res.render("signup", {
+              isAuth: isAuth,
               title: "Sign up",
               errorMessage: [err.sqlMessage],
             });
           });
       } else {
         res.render("signup", {
+          isAuth: isAuth,
           title: "Sign up",
           errorMessage: ["パスワードが一致しません"],
         });
@@ -45,6 +53,7 @@ router.post('/', function (req, res, next) {
     .catch(function (err) {
       console.error(err);
       res.render("signup", {
+        isAuth: isAuth,
         title: "Sign up",
         errorMessage: [err.sqlMessage],
       });
